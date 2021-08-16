eec3feab450ad9fcf0a40d06439430e9379d4f58
jackjohnson-tred <83763241+jackjohnson-tred@users.noreply.github.com>
2021-08-16 11:56:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ try {
   const AWS_SECRET_KEY = core.getInput('AWS_SECRET_KEY');
   const AWS_SECRET_ID = core.getInput('AWS_SECRET_ID');
   const AWS_REGION = core.getInput('AWS_REGION');
+  if (core.getInput('revision-id')) {
+    const revisionId = core.getInput('revision-id');
+  }
 
   console.log(`Deploying ${functionName} from ${package}.`);
 
@@ -30,6 +33,9 @@ try {
     Publish: true,
     ZipFile: zipBuffer,
   };
+  if (revisionId) {
+    params.RevisionId = revisionId;
+  }
 
   lambda.updateFunctionCode(params, err => {
       if (err) {
